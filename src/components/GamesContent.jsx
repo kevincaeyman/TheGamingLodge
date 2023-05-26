c3f3c0546db6bd6e7de88b92b6d6c6c2473a28dd
kevincaeyman <kevin.caeyman@gmail.com>
2023-05-26 18:22:30 +0200

    New Releases filter functionality done, Filter by platform filter in progress

--- src/components/GamesContent.jsx
+++ src/components/GamesContent.jsx
@@ -1,26 +1,40 @@
 import React, { useState, useEffect, useContext } from "react";
 import { ApiContext } from "../context/ApiContext";
-import { PLAYSTATION_5_PLATFORM, PLAYSTATION_4_PLATFORM, XBOX_SERIES_PLATFORM, XBOX_ONE_PLATFORM, NINTENDO_SWITCH_PLATFORM, NINTENDO_3DS_PLATFORM, PC_PLATFORM, MAC_PLATFORM, LINUX_PLATFORM, IOS_PLATFORM, ANDROID_PLATFORM } from "../constants/platforms";
-import closingIcon from "../assets/closingIcon.png"
+import {
+  PLAYSTATION_5_PLATFORM,
+  PLAYSTATION_4_PLATFORM,
+  XBOX_SERIES_PLATFORM,
+  XBOX_ONE_PLATFORM,
+  NINTENDO_SWITCH_PLATFORM,
+  NINTENDO_3DS_PLATFORM,
+  PC_PLATFORM,
+  WII_U_PLATFORM,
+} from "../constants/platforms";
+import closingIcon from "../assets/closingIcon.png";
 
 const GamesContent = () => {
-  const { gameData, retrieveNewReleases, newReleases } = useContext(ApiContext);
+  const {
+    gameData,
+    retrieveNewReleases,
+    newReleases,
+    retrieveAllGames,
+  } = useContext(ApiContext);
   const [currentView, setCurrentView] = useState("default");
   const [filteredGames, setFilteredGames] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPlatform, setSelectedPlatform] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
-  const [offsetNewReleases, setOffsetNewReleases] = useState(0)
-  const [selectedGame, setSelectedGame] = useState(null)
+  const [offsetAllGames, setOffsetAllGames] = useState(0);
+  const [offsetNewReleases, setOffsetNewReleases] = useState(0);
+  const [selectedGame, setSelectedGame] = useState(null);
 
   useEffect(() => {
     if (gameData && gameData.results) {
       filterGames();
       setLoading(false);
-      console.log(gameData.results)
+      console.log(gameData.results);
     }
-  }, [gameData, currentView, currentPage]);
+  }, [gameData, currentView]);
 
   useEffect(() => {
     const filterBySearchQuery = () => {
@@ -37,6 +51,10 @@ const GamesContent = () => {
 
   useEffect(() => {
     filterGames();
+  }, [currentView, selectedPlatform]);
+
+  useEffect(() => {
+    filterByPlatform();
   }, [selectedPlatform]);
 
   const filterGames = () => {
@@ -50,17 +68,18 @@ const GamesContent = () => {
   };
 
   const filterNewReleases = () => {
-    retrieveNewReleases();
-    setOffsetNewReleases((prev) => prev + 100)
+    setOffsetNewReleases((prev) => prev + 100);
   };
 
   const filterByPlatform = () => {
-    const filtered = gameData.results.filter(
-      (game) =>
-        game.platform &&
-        game.platform.some((platform) => platform.name === selectedPlatform)
-    );
-    setFilteredGames(filtered);
+    if (gameData && gameData.results) {
+      const filtered = gameData.results.filter((game) => {
+        return game.platforms.some(
+          (platform) => platform.id.toString() === selectedPlatform
+        );
+      });
+      setFilteredGames(filtered);
+    }
   };
 
   const handlePlatformChange = (e) => {
@@ -74,28 +93,44 @@ const GamesContent = () => {
   };
 
   const loadMoreGames = () => {
-    retrieveNewReleases(offsetNewReleases);
-    setOffsetNewReleases((prev) => prev + 100)
+    setOffsetAllGames((prev) => prev + 40);
+  };  
+
+  const loadMoreNewReleases = () => {
+    setOffsetNewReleases((prev) => prev + 100);
   };
 
   const SelectedGame = ({ game }) => {
-    const platforms = game.platforms && game.platforms.map(platform => platform.name).join(", ");
+    const platforms =
+      game.platforms &&
+      game.platforms.map((platform) => platform.name).join(", ");
 
     return (
       <div className="selectedGame">
         <div className="selectedGameHeader">
-        <p className="selectedGameName"> <b> {game.name && game.name}</b></p>
-        <img src={closingIcon} className="closingIcon" onClick={unselectGame}/>
+          <p className="selectedGameName">
+            <b>{game.name && game.name}</b>
+          </p>
+          <img
+            src={closingIcon}
+            className="closingIcon"
+            onClick={unselectGame}
+            alt="Closing Icon"
+          />
         </div>
         <img
-          src={game.image && game.image.original_url}
+          src={game.image && game.image.super_url}
           alt="Selected Game Image"
           className="selectedGameImage"
         />
         <div className="selectedGameDetails">
-          <img src=""/>
-          <p className="selectedGameDescription"> <b>Description:</b> <br/><br/> {game.deck}</p>
-          <p className="selectedGamePlatforms"> <b>Platforms:</b> <br/> <br/> {platforms}</p>
+          <p className="selectedGameDescription">
+            <b>Description:</b> <br />
+            <br /> {game.deck}
+          </p>
+          <p className="selectedGamePlatforms">
+            <b>Platforms:</b> <br /> <br /> {platforms}
+          </p>
         </div>
       </div>
     );
@@ -104,9 +139,22 @@ const GamesContent = () => {
   const selectGame = (game) => {
     setSelectedGame(game);
   };
+
   const unselectGame = () => {
-    setSelectedGame(null)
-  }
+    setSelectedGame(null);
+  };
+
+  useEffect(() => {
+    if (offsetAllGames > 0) {
+      retrieveAllGames(offsetAllGames, true);
+    }
+  }, [offsetAllGames]);  
+
+  useEffect(() => {
+    if (offsetNewReleases > 0) {
+      retrieveNewReleases(offsetNewReleases);
+    }
+  }, [offsetNewReleases]);
 
   return (
     <div className="gamesContent">
@@ -114,8 +162,8 @@ const GamesContent = () => {
       <h1>Games</h1>
       {currentView === "default" && (
         <p className="gameIntro">
-          The following list displays the games by upcoming date. In order to look for a specific
-          game, please use the following options:
+          The following list displays the games by upcoming date. In order to
+          look for a specific game, please use the following options:
         </p>
       )}
       <input
@@ -137,13 +185,14 @@ const GamesContent = () => {
           <option value="" key="default">
             Filter by platform
           </option>
-          <option value={PLAYSTATION_5_PLATFORM} >PlayStation 5</option>
-          <option value={XBOX_SERIES_PLATFORM}>Xbox Series XS</option>
-          <option value={PLAYSTATION_4_PLATFORM}>PlayStation 4</option>
-          <option value={XBOX_ONE_PLATFORM}>Xbox One</option>
+          <option value={PLAYSTATION_5_PLATFORM}>PlayStation 5</option>
           <option value={NINTENDO_SWITCH_PLATFORM}>Nintendo Switch</option>
+          <option value={PLAYSTATION_4_PLATFORM}>PlayStation 4</option>
+          <option value={XBOX_SERIES_PLATFORM}>Xbox Series X|S</option>
+          <option value={XBOX_ONE_PLATFORM}>Xbox One</option>
           <option value={PC_PLATFORM}>PC</option>
-          {/* Add more options for other platforms */}
+          <option value={NINTENDO_3DS_PLATFORM}>Nintendo 3DS</option>
+          <option value={WII_U_PLATFORM}>Wii U</option>
         </select>
 
         {/* Button to show a random game */}
@@ -160,19 +209,17 @@ const GamesContent = () => {
         ) : (
           <>
             {currentView === "default" &&
-              gameData.results.map((game) => (
+              filteredGames.map((game) => (
                 <div
                   key={game.id}
                   className="gameCard"
                   onClick={() => selectGame(game)}
                 >
-                  <div key={game.id}>
-                    <img
-                      src={game.image && game.image.original_url}
-                      alt="Game Image"
-                    />
-                    <p className="gameName">{game.name && game.name}</p>
-                  </div>
+                  <img
+                    src={game.image && game.image.super_url}
+                    alt="Game Image"
+                  />
+                  <p className="gameName">{game.name && game.name}</p>
                 </div>
               ))}
             {newReleases.length === 0 && currentView !== "default" ? (
@@ -185,13 +232,11 @@ const GamesContent = () => {
                     className="gameCard"
                     onClick={() => selectGame(game)}
                   >
-                    <div key={game.id}>
-                      <img
-                        src={game.image && game.image.original_url}
-                        alt="Game Image"
-                      />
-                      <p className="gameName">{game.name && game.name}</p>
-                    </div>
+                    <img
+                      src={game.image && game.image.super_url}
+                      alt="Game Image"
+                    />
+                    <p className="gameName">{game.name && game.name}</p>
                   </div>
                 ))}
               </>
@@ -199,13 +244,6 @@ const GamesContent = () => {
           </>
         )}
       </div>
-
-      {/* Button to load more games */}
-      {currentView !== "default" && (
-        <button className="loadMoreButton" onClick={loadMoreGames}>
-          Load More Games
-        </button>
-      )}
     </div>
   );
 };
--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -6,33 +6,49 @@ const ApiContext = createContext();
 const ApiProvider = ({ children }) => {
   const [gameData, setGameData] = useState(null);
   const [platforms, setPlatforms] = useState([]);
-  const [newReleases, setNewReleases] = useState([])
+  const [newReleases, setNewReleases] = useState([]);
+  const [newReleasesOffset, setNewReleasesOffset] = useState(0);
 
   const retrieveAllGames = async (offset) => {
     try {
       const response = await axios.get(
         "https://www.giantbomb.com/api/games/",
         {
+          withCredentials: true,
           params: {
             api_key: "e32053903724a60a9020ddb3666371d879f12488",
             format: "json",
             query: "games",
             resources: "game",
-            filter: "platforms:176,146,35,19,179,145,20,157,117,139,36",
+            filter: "platforms:157,176,146,179,145,139,117,94,130",
             sort: "original_release_date:desc",
-            offset:offset
+            limit: 40,
+            offset: offset,
           },
         }
       );
-      setGameData(response.data)
-      const uniquePlatforms = response.data.results
-        .flatMap((game) => game.platforms)
-        .filter((platform, index, platforms) => {
-          return (
-            platforms.findIndex((p) => p.id === platform.id) === index
+
+      setGameData((prevGameData) => {
+        const newGameData = response.data;
+        if (prevGameData) {
+          newGameData.results = prevGameData.results.concat(
+            newGameData.results
           );
-        });
-      setPlatforms(uniquePlatforms);
+        }
+        return newGameData;
+      });
+
+      setPlatforms((prevPlatforms) => {
+        const uniquePlatforms = response.data.results
+          .flatMap((game) => game.platforms)
+          .filter((platform, index, platforms) => {
+            return (
+              platforms.findIndex((p) => p.id === platform.id) === index
+            );
+          });
+
+        return prevPlatforms.concat(uniquePlatforms);
+      });
     } catch (error) {
       console.log(error);
     }
@@ -48,31 +64,79 @@ const ApiProvider = ({ children }) => {
             format: "json",
             query: "games",
             resources: "game",
-            filter: "platforms:176,146,35,19,179,145,20,157,117,139,36",
+            filter: "platforms:157,176,146,179,145,139,117,94,130",
             sort: "original_release_date:desc",
-            offset:offset
+            limit: 40,
+            offset: offset,
           },
         }
       );
-      const results = newReleases.concat(response.data.results)
-      const filtered = results.filter((game) => {
+
+      const newResults = response.data.results.filter((game) => {
         const releaseDate = game.original_release_date;
         const today = new Date().toISOString().split("T")[0];
         return releaseDate && releaseDate <= today && game.name;
       });
-      setNewReleases(filtered)
+
+      setNewReleases((prevReleases) => {
+        const updatedReleases = prevReleases.concat(newResults);
+        const uniqueReleases = Array.from(
+          new Set(updatedReleases.map((game) => game.id))
+        ).map((id) => updatedReleases.find((game) => game.id === id));
+        return uniqueReleases;
+      });
     } catch (error) {
       console.log(error);
     }
-  }
+  };
+
+  const handleScroll = () => {
+    const windowHeight =
+      "innerHeight" in window
+        ? window.innerHeight
+        : document.documentElement.offsetHeight;
+    const body = document.body;
+    const html = document.documentElement;
+    const docHeight = Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight
+    );
+    const windowBottom = windowHeight + window.pageYOffset;
+    if (windowBottom >= docHeight) {
+      const offset = newReleasesOffset + 100;
+      setNewReleasesOffset(offset);
+      retrieveNewReleases(offset);
+    }
+  };
 
   useEffect(() => {
-    
-    retrieveAllGames();
+    retrieveAllGames(0);
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
+  useEffect(() => {
+    if (gameData && gameData.results.length > 0) {
+      const offset = gameData.results.length;
+      retrieveAllGames(offset);
+    }
+  }, [gameData]);
+
   return (
-    <ApiContext.Provider value={{ gameData, platforms, retrieveNewReleases, newReleases }}>
+    <ApiContext.Provider
+      value={{
+        gameData,
+        platforms,
+        retrieveNewReleases,
+        newReleases,
+        retrieveAllGames,
+      }}
+    >
       {children}
     </ApiContext.Provider>
   );
